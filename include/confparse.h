30ae21a037ca2ec4f83078eb52ad71bd6f6d5346
Raphaël Barrois <raphael.barrois@polytechnique.org>
2012-05-09 00:53:48 +0200

    Renumber configuration options.
    Signed-off-by: Raphaël Barrois <raphael.barrois@polytechnique.org>

--- include/confparse.h
+++ include/confparse.h
@@ -75,49 +75,49 @@ struct ConfVar
 /* subtokens */
 
 #define SCONFT_NAME     "NAME"
-#define SCONFF_NAME                 0x000001
+#define SCONFF_NAME                 1
 #define SCONFT_INFO     "INFO"
-#define SCONFF_INFO                 0x000002
+#define SCONFF_INFO                 2
 #define SCONFT_DPASS    "DPASS"
-#define SCONFF_DPASS                0x000004
+#define SCONFF_DPASS                3
 #define SCONFT_RPASS    "RPASS"
-#define SCONFF_RPASS                0x000008
+#define SCONFF_RPASS                4
 #define SCONFT_PINGFREQ "PINGFREQ"
-#define SCONFF_PINGFREQ             0x000010
+#define SCONFF_PINGFREQ             5
 #define SCONFT_CONNFREQ "CONNFREQ"
-#define SCONFF_CONNFREQ             0x000020
+#define SCONFF_CONNFREQ             6
 #define SCONFT_MAXUSERS "MAXUSERS"
-#define SCONFF_MAXUSERS             0x000040
+#define SCONFF_MAXUSERS             7
 #define SCONFT_MAXSENDQ "MAXSENDQ"
-#define SCONFF_MAXSENDQ             0x000080
+#define SCONFF_MAXSENDQ             8
 #define SCONFT_CLASS     "CLASS"
-#define SCONFF_CLASS                0x000100
+#define SCONFF_CLASS                9
 #define SCONFT_HOST     "HOST"
-#define SCONFF_HOST                 0x000200
+#define SCONFF_HOST                 10
 #define SCONFT_PORT     "PORT"
-#define SCONFF_PORT                 0x000800
+#define SCONFF_PORT                 11
 #define SCONFT_PASSWD   "PASSWD"
-#define SCONFF_PASSWD               0x001000
+#define SCONFF_PASSWD               12
 #define SCONFT_ACCESS   "ACCESS"
-#define SCONFF_ACCESS               0x002000
+#define SCONFF_ACCESS               13
 #define SCONFT_BIND     "BIND"
-#define SCONFF_BIND                 0x004000
+#define SCONFF_BIND                 14
 #define SCONFT_APASSWD  "APASSWD"
-#define SCONFF_APASSWD              0x008000
+#define SCONFF_APASSWD              15
 #define SCONFT_CPASSWD  "CPASSWD"
-#define SCONFF_CPASSWD              0x010000
+#define SCONFF_CPASSWD              16
 #define SCONFT_FLAGS    "FLAGS"
-#define SCONFF_FLAGS                0x020000
+#define SCONFF_FLAGS                17
 #define SCONFT_REASON   "REASON"
-#define SCONFF_REASON               0x040000
+#define SCONFF_REASON               18
 #define SCONFT_TYPE     "TYPE"
-#define SCONFF_TYPE                 0x080000
+#define SCONFF_TYPE                 19
 #define SCONFT_MASK     "MASK"
-#define SCONFF_MASK                 0x100000
+#define SCONFF_MASK                 20
 #define SCONFT_IPMASK   "IPMASK"
-#define SCONFF_IPMASK               0x200000
+#define SCONFF_IPMASK               21
 
-#define SCONFF_STRING               0x800000    /* allow freeform strings */
+#define SCONFF_STRING               22    /* allow freeform strings */
 
 /* subtoken aliases */
 
@@ -127,96 +127,96 @@ struct ConfVar
 /* these are the strings for options ONLY */
 
 #define OPTT_NETNAME    "NETWORK_NAME"
-#define OPTF_NETNAME                0x000002
+#define OPTF_NETNAME                1
 #define OPTT_STAFFADDY  "STAFF_ADDRESS"
-#define OPTF_STAFFADDY              0x000004
+#define OPTF_STAFFADDY              2
 #define OPTT_SERVNAME   "SERVICES_NAME"
-#define OPTF_SERVNAME               0x000010
+#define OPTF_SERVNAME               3
 #define OPTT_MAXCHAN    "MAXCHANNELS"
-#define OPTF_MAXCHAN                0x000020
+#define OPTF_MAXCHAN                4
 #define OPTT_WGMONHOST  "WGMONHOST"
-#define OPTF_WGMONHOST              0x000040
+#define OPTF_WGMONHOST              5
 #define OPTT_WGMONURL   "WGMONURL"
-#define OPTF_WGMONURL               0x000080
+#define OPTF_WGMONURL               6
 #define OPTT_NKLINEADDY "NETWORK_KLINE"
-#define OPTF_NKLINEADDY             0x000100
+#define OPTF_NKLINEADDY             7
 #define OPTT_LKLINEADDY "LOCAL_KLINE"
-#define OPTF_LKLINEADDY             0x000200
+#define OPTF_LKLINEADDY             8
 #define OPTT_CRYPTPASS  "CRYPT_OPER_PASS"
-#define OPTF_CRYPTPASS              0x000400
+#define OPTF_CRYPTPASS              9
 #define OPTT_SMOTD      "SHORT_MOTD"
-#define OPTF_SMOTD                  0x000800
+#define OPTF_SMOTD                  10
 #define OPTT_SERVTYPE   "SERVTYPE"
-#define OPTF_SERVTYPE               0x001000
+#define OPTF_SERVTYPE               11
 #define OPTT_STATSNAME  "STATS_NAME"
-#define OPTF_STATSNAME              0x002000
+#define OPTF_STATSNAME              12
 #define OPTT_TSMAXDELTA "TS_MAX_DELTA"
-#define OPTF_TSMAXDELTA             0x004000
+#define OPTF_TSMAXDELTA             13
 #define OPTT_TSWARNDELTA "TS_WARN_DELTA"
-#define OPTF_TSWARNDELTA            0x008000
+#define OPTF_TSWARNDELTA            14
 #define OPTT_NSREGURL   "NSHELPURL"
-#define OPTF_NSREGURL               0x010000
+#define OPTF_NSREGURL               15
 #define OPTT_SHOWLINKS  "SHOW_LINKS"
-#define OPTF_SHOWLINKS              0x020000
+#define OPTF_SHOWLINKS              16
 #define OPTT_SPLITOPOK  "ALLOW_SPLIT_OPS"
-#define OPTF_SPLITOPOK              0x040000
+#define OPTF_SPLITOPOK              17
 #define OPTT_LCLONES    "LOCAL_CLONES"
-#define OPTF_LCLONES                0x080000
+#define OPTF_LCLONES                18
 #define OPTT_GCLONES    "GLOBAL_CLONES"
-#define OPTF_GCLONES                0x100000
+#define OPTF_GCLONES                19
 #define OPTT_DOMAIN     "HOST_DOMAIN"
-#define OPTF_DOMAIN                 0x1000000
+#define OPTF_DOMAIN                 20
 #define OPTT_HOSTPREFIX "HOST_PREFIX"
-#define OPTF_HOSTPREFIX             0x2000000
+#define OPTF_HOSTPREFIX             21
 #define OPTT_AUTOUMODEV "AUTO_UMODE_V"
-#define OPTF_AUTOUMODEV             0x4000000
+#define OPTF_AUTOUMODEV             22
 #define OPTT_AUTOUMODER "AUTO_UMODE_R"
-#define OPTF_AUTOUMODER             0x8000000
+#define OPTF_AUTOUMODER             23
 #define OPTT_HIDDENSERVNAME    "HIDDENSERVNAME"
-#define OPTF_HIDDENSERVNAME         0x10000000
+#define OPTF_HIDDENSERVNAME         24
 #define OPTT_HIDDENSERVDESC    "HIDDENSERVDESC"
-#define OPTF_HIDDENSERVDESC         0x20000000
+#define OPTF_HIDDENSERVDESC         25
 #define OPTT_BOTCLASS         "BOT_CLASS"
-#define OPTF_BOTCLASS              0x200000
+#define OPTF_BOTCLASS              26
 #define OPTT_HELPCHAN             "HELPCHAN"
-#define OPTF_HELPCHAN              0x300000
+#define OPTF_HELPCHAN              27
 #define OPTT_WEBSITE             "WEBSITE"
-#define OPTF_WEBSITE              0x400000
+#define OPTF_WEBSITE              28
 #define OPTT_AUP                 "AUP"
-#define OPTF_AUP                  0x500000
+#define OPTF_AUP                  29
 #define OPTT_RQUIT                "rquit"
-#define OPTF_RQUIT                  0x00800000
+#define OPTF_RQUIT                  30
 
 
 
 /* module block definitions */
 
 #define MBTT_PATH       "PATH"
-#define MBTF_PATH                   0x0001
+#define MBTF_PATH                   1
 #define MBTT_AUTOLOAD   "AUTOLOAD"
-#define MBTF_AUTOLOAD               0x0002
+#define MBTF_AUTOLOAD               2
 #define MBTT_OPTLOAD    "OPTLOAD"
-#define MBTF_OPTLOAD                0x0004
+#define MBTF_OPTLOAD                3
 
 /* our variable types */
 
 /* ssl block definitions */
 
 #define SSTT_CERTIFICATE  "CERTIFICATE"
-#define SSTF_CERTIFICATE            0x0001
+#define SSTF_CERTIFICATE            1
 #define SSTT_KEYFILE      "KEYFILE"
-#define SSTF_KEYFILE                0x0002
+#define SSTF_KEYFILE                2
 #define SSTT_UMODEZ       "allow_umodez"
-#define SSTF_UMODEZ                 0x0004
+#define SSTF_UMODEZ                 3
 
 
 /* elmer block */
 
 #ifdef TOYS
 #define EETT_STRING        "STRING"
-#define EETF_STRING                 0x0001
+#define EETF_STRING                 1
 #define EETT_TRANSLATE     "TRANSLATE"
-#define EETF_TRANSLATE              0x0002
+#define EETF_TRANSLATE              2
 #endif
 
 #define VARTYPE_INT     0x0001  /* integers             */
